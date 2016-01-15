e4b6b363aa5cf9a772e9880a909f8159b7ba8601
Buffy Miller <buffy@sethmiller.com>
2016-01-15 07:50:56 -0500

    Patch bug in aws-sdk-v1
    aws-sdk-v1 does not downcase the canonical headers like it needs to for
    proper signing - this fixed that.

--- lib/faraday_middleware/aws_signers_v4_ext.rb
+++ lib/faraday_middleware/aws_signers_v4_ext.rb
@@ -1,13 +1,21 @@
-require 'aws-sdk-core/signers/v4'
+require 'aws/core/signers/version_4'
 
 module AwsSignersV4Ext
-  def signed_headers(request)
-    super.downcase
+  def canonical_headers req
+    headers = []
+    req.headers.each_pair do |k,v|
+      k = k.downcase
+      headers << [k,v] unless k == 'authorization'
+    end
+    headers = headers.sort_by(&:first)
+    headers.map{|k,v| "#{k}:#{canonical_header_values(v)}" }.join("\n")
   end
 end
 
-class Aws::Signers::V4
-  unless Aws::Signers::V4 < AwsSignersV4Ext
+class AWS::Core::Signers::Version4
+  unless AWS::Core::Signers::Version4 < AwsSignersV4Ext
     prepend AwsSignersV4Ext
   end
 end
+
+
